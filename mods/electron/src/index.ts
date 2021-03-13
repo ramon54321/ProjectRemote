aa866327fed45148e805fe012d04fe0c072705d7
Ramon Brand <ramon2009@gmail.com>
2021-03-13 11:48:06 +0200

    Cleanup update

--- mods/electron/src/index.ts
+++ mods/electron/src/index.ts
@@ -10,10 +10,26 @@ import * as Utils from './utils'
 const uiCtx = new UICtx(800, 800, update, draw)
 
 function update(delta: number) {
+  handleSetActionStateToEntity(uiCtx)
+  handleClick(uiCtx)
+  handleSpacebar(uiCtx)
+  handleCamera(delta, uiCtx)
+}
+
+function draw(state: NetworkState) {
+  if (uiCtx.toolbar.isChecked('WorldSurface')) drawWorldSurface(uiCtx, state)
+  drawHoverTile(uiCtx)
+  if (uiCtx.toolbar.isChecked('Heat')) drawHeat(uiCtx, state)
+  if (uiCtx.toolbar.isChecked('Entities')) drawEntities(uiCtx, state)
+}
+
+function handleSetActionStateToEntity(uiCtx: UICtx) {
   if (uiCtx.isActionState('Select') && uiCtx.getSelectedEntity()) {
     uiCtx.setActionState('Entity')
   }
+}
 
+function handleClick(uiCtx: UICtx) {
   if (uiCtx.getKeyDown('click')) {
     if (uiCtx.callbacks.isDirtyClick()) {
       const hover = Utils.getHover(uiCtx)
@@ -30,11 +46,15 @@ function update(delta: number) {
       uiCtx.flagRedraw()
     }
   }
+}
 
+function handleSpacebar(uiCtx: UICtx) {
   if (uiCtx.getKeyOnce(' ') && uiCtx.isActionState('Entity')) {
     uiCtx.events.emit('promptEntity')
   }
+}
 
+function handleCamera(delta: number, uiCtx: UICtx) {
   if (uiCtx.getKeyDown('a')) {
     uiCtx.moveCameraX(-500, delta)
   }
@@ -47,11 +67,4 @@ function update(delta: number) {
   if (uiCtx.getKeyDown('s')) {
     uiCtx.moveCameraY(500, delta)
   }
-}
-
-function draw(state: NetworkState) {
-  if (uiCtx.toolbar.isChecked('WorldSurface')) drawWorldSurface(uiCtx, state)
-  drawHoverTile(uiCtx)
-  if (uiCtx.toolbar.isChecked('Heat')) drawHeat(uiCtx, state)
-  if (uiCtx.toolbar.isChecked('Entities')) drawEntities(uiCtx, state)
 }
